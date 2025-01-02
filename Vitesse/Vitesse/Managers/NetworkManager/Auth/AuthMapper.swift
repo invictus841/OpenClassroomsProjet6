15819967c2746e4d783dbf74ac0f2f234604d930
invictus841 <usedforfunny@gmail.com>
2025-01-02 23:15:36 +0100

    Added AuthMapper.

---
+++ Vitesse/Vitesse/Managers/NetworkManager/Auth/AuthMapper.swift
@@ -0,0 +1,24 @@
+//
+//  AuthMapper.swift
+//  Vitesse
+//
+//  Created by Alexandre Talatinian on 02/01/2025.
+//
+
+import Foundation
+
+enum AuthMapper {
+    struct LoginResponse: Decodable {
+        let token: String
+        let isAdmin: Bool
+    }
+    
+    static func map(_ data: Data, and response: HTTPURLResponse) throws -> (token: String, isAdmin: Bool) {
+        guard response.statusCode == 200,
+              let loginResponse = try? JSONDecoder().decode(LoginResponse.self, from: data) else {
+            throw NetworkError.invalidResponse
+        }
+        
+        return (loginResponse.token, loginResponse.isAdmin)
+    }
+}
