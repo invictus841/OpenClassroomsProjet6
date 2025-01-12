155f33105d43c2e0867c84f99bf69f689fc5905b
invictus841 <usedforfunny@gmail.com>
2025-01-12 01:27:04 +0100

    AppViewModel updated

--- Vitesse/Vitesse/ViewModels/AppViewModel.swift
+++ Vitesse/Vitesse/ViewModels/AppViewModel.swift
@@ -9,26 +9,30 @@ import Foundation
 
 final class AppViewModel: ObservableObject {
     @Published var isLogged: Bool
+    @Published var isAdmin: Bool
     
     init() {
         isLogged = false
+        isAdmin = false
     }
     
     var authenticationViewModel: LoginViewModel {
-        return LoginViewModel { [weak self] in
-            DispatchQueue.main.async {  // Thread safety here
+        return LoginViewModel { [weak self] isAdmin in
+            DispatchQueue.main.async {
+                print("Login successful - Setting isAdmin to: \(isAdmin)")
                 self?.isLogged = true
+                self?.isAdmin = isAdmin
             }
         }
     }
     
     var registerViewModel: RegisterViewModel {
-            return RegisterViewModel {
-                // After successful registration, we could either:
-                // 1. Automatically log them in (would need to modify RegisterService to return a token)
-                // 2. Take them back to login screen
-                print("Registration successful")
-                // For now, let's just take them back to login
-            }
+        return RegisterViewModel {_ in 
+            print("Registration successful")
         }
+    }
+    
+    var candidatesViewModel: CandidateViewModel {
+        return CandidateViewModel(isAdmin: isAdmin)
+    }
 }
