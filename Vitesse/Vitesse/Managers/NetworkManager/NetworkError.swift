4806a02bd0dbe54447e1ed1cddddfcbda12ce153
invictus841 <usedforfunny@gmail.com>
2025-01-12 01:28:04 +0100

    NetworkError file updated

--- Vitesse/Vitesse/Managers/NetworkManager/NetworkError.swift
+++ Vitesse/Vitesse/Managers/NetworkManager/NetworkError.swift
@@ -7,10 +7,25 @@
 
 import Foundation
 
-enum NetworkError: Error {
+enum NetworkError: LocalizedError, Equatable {
     case invalidResponse
     case invalidData
     case invalidURL
     case unauthorized
     case serverError(String)
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidResponse:
+            return "Invalid response from server"
+        case .invalidData:
+            return "Invalid data received"
+        case .invalidURL:
+            return "Invalid URL"
+        case .unauthorized:
+            return "Unauthorized access"
+        case .serverError(let message):
+            return message
+        }
+    }
 }
